135d3e8ae4cdeae0c6cbc8124ed974132c03af76
hernsys <horacioantar@gmail.com>
2013-08-22 01:03:11 -0300

    startup learn jbpm

---
+++ src/main/java/com/hernsys/model/Person.java
@@ -0,0 +1,23 @@
+package com.hernsys.model;
+
+public class Person {
+	
+	private String name;
+	
+	public Person(){
+		
+	}
+	
+	public Person(String name){
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
